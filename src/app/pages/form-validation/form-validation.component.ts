c25e9362310d3faaa7f2aac6bb254ba557bd1c4e
Eshwar <eshwargowda19@gmail.com>
2019-09-06 15:13:40 +0530

    form validation

---
+++ src/app/pages/form-validation/form-validation.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-validation',
+  templateUrl: './form-validation.component.html',
+  styleUrls: ['./form-validation.component.scss']
+})
+export class FormValidationComponent implements OnInit {
+
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {debugger;
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+        return;
+    }
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+  }
+
+}
